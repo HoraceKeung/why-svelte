24e47fb080c1e5df2b0542383b28cacdbed63f7f
Horace Keung <horacekeung@hotmail.com>
2019-07-26 23:59:44 +0100

    add entry

--- src/App.svelte
+++ src/App.svelte
@@ -8,7 +8,7 @@
 	<div class="container pt-8">
 		<div class="flex flex-wrap -mx-2">
 			{#each resources as r}
-			<div class="w-full sm:w-1/2 lg:w-1/3 xl:w-1/4 px-2 mb-4">
+			<div class="w-full sm:w-1/2 lg:w-1/3 xl:w-1/4 px-2 mb-6">
 				<div class="shadow-md grad w-full h-full rounded flex flex-col overflow-hidden">
 					<a target="_blank" href={r.link}><div class="h-48 bg-contain bg-center bg-no-repeat" style=""/></a>
 					<div class="p-4 flex flex-col flex-1 bg-white">
@@ -36,6 +36,30 @@
 <script>
 const resources = [
 	{
+		title: 'Introduction to Svelte',
+		link: 'https://daveceddia.com/svelte-intro/',
+		date: '24 Jul 2019',
+		tags: [
+			'text',
+			'vue',
+			'react',
+			'angular'
+		]
+	},
+	{
+		title: 'Web Development Simplified with Svelte',
+		link: 'https://objectcomputing.com/resources/publications/sett/july-2019-web-dev-simplified-with-svelte',
+		date: '24 Jul 2019',
+		tags: [
+			'text',
+			'sapper',
+			'vue',
+			'react',
+			'angular',
+			'nativescript'
+		]
+	},
+	{
 		title: 'Svelte for Angular Developers',
 		link: 'https://blog.bitsrc.io/svelte-for-angular-developers-34012b93420c',
 		date: '24 Jul 2019',
@@ -52,6 +76,139 @@ const resources = [
 		date: '22 Jul 2019',
 		tags: [
 			'text'
+		]
+	},
+	{
+		title: 'Exploring Sapper + Svelte: A quick tutorial',
+		link: 'https://blog.logrocket.com/exploring-sapper-svelte-a-quick-tutorial/',
+		date: '2 Jul 2019',
+		tags: [
+			'text',
+			'sapper',
+			'react'
+		]
+	},
+	{
+		title: "Svelte is the most beautiful web framework I've ever seen",
+		link: 'https://dev.to/jesseskinner/svelte-is-the-most-beautiful-web-framework-i-ve-ever-seen-325f',
+		date: '29 Jun 2019',
+		tags: [
+			'text'
+		]
+	},
+	{
+		title: 'Why SvelteJS may be the best framework for new web devs',
+		link: 'https://dev.to/bholmesdev/why-sveltejs-may-be-the-best-framework-for-new-web-devs-205i',
+		date: '28 Jun 2019',
+		tags: [
+			'text',
+			'vue',
+			'react',
+			'angular'
+		]
+	},
+	{
+		title: 'Getting started with Svelte',
+		link: 'https://blog.knoldus.com/getting-started-with-svelte/',
+		date: '27 Jun 2019',
+		tags: [
+			'text',
+			'vue',
+			'react'
+		]
+	},
+	{
+		title: 'Why Svelte is the best JS "framework"',
+		link: 'https://www.youtube.com/watch?v=rMBusx68ujY',
+		date: '26 Jun 2019',
+		tags: [
+			'video'
+		]
+	},
+	{
+		title: 'The importance of elegance',
+		link: 'https://www.varvet.com/blog/the-importance-of-elegance/',
+		date: '25 Jun 2019',
+		tags: [
+			'text'
+		]
+	},
+	{
+		title: "Svelte 3's radical new approach to web frameworks",
+		link: 'https://devmode.fm/episodes/svelte-3s-radical-new-approach-to-web-frameworks',
+		date: '24 Jun 2019',
+		tags: [
+			'audio'
+		]
+	},
+	{
+		title: "Reactive user interfaces using SvelteJS",
+		link: 'https://denisjakus.com/reactive-user-interfaces-using-sveltejs/',
+		date: '19 Jun 2019',
+		tags: [
+			'text'
+		]
+	},
+	{
+		title: 'Svelte.js: Two way binding',
+		link: 'https://medium.com/@Roli_Dori/svelte-js-two-way-binding-ded98a89af1c',
+		date: '25 May 2019',
+		tags: [
+			'text',
+			'vue',
+			'react'
+		]
+	},
+	{
+		title: 'Truly reactive programming with Svelte 3.0',
+		link: 'https://blog.logrocket.com/truly-reactive-programming-with-svelte-3-0-321b49b75969/',
+		date: '21 May 2019',
+		tags: [
+			'text',
+			'react',
+			'angular'
+		]
+	},
+	{
+		title: 'Svelte vs React vs Angular vs Vue',
+		link: 'https://www.youtube.com/watch?v=DZyWNS4fVE0',
+		date: '15 May 2019',
+		tags: [
+			'video',
+			'vue',
+			'react',
+			'angular'
+		]
+	},
+	{
+		title: 'Svelte 3 Front-End Framework Moves Reactivity into the JavaScript Language, Q&A with Rich Harris',
+		link: 'https://www.infoq.com/news/2019/05/svelte-3-interview-rich-harris/',
+		date: '6 May 2019',
+		tags: [
+			'text',
+			'sapper',
+			'vue',
+			'react',
+			'nativescript'
+		]
+	},
+	{
+		title: 'Svelte 3 Is All Kinds Of Awesome...',
+		link: 'https://www.youtube.com/watch?v=efAMY1JvrcQ',
+		date: '30 Apr 2019',
+		tags: [
+			'video'
+		]
+	},
+	{
+		title: 'Svelte 3 Reaction & QuickStart Tutorial',
+		link: 'https://www.youtube.com/watch?v=043h4ugAj4c',
+		date: '25 Apr 2019',
+		tags: [
+			'video',
+			'vue',
+			'react',
+			'angular'
 		]
 	},
 	{
