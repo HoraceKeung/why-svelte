2dbcb12278383d16fc17c0243ed8062d3921d0a9
Horace Keung <horacekeung@hotmail.com>
2019-07-28 21:17:12 +0100

    update meta scraping

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,19 @@
 const functions = require('firebase-functions')
-const cors = require('cors')({origin: true})
+const cors = require('cors')({origin: ['http://localhost:3000', 'https://why-svelte-js.web.app']})
 const cheerio = require('cheerio')
 const fetch = require('node-fetch')
 
 const scrapeMetatags = urls => {
 	const requests = urls.map(async url => {
-		const res = await fetch(url)
+		const res = await fetch(url).catch(err => {err})
 		const html = await res.text()
 		const $ = cheerio.load(html)
 		const getMetatag = name =>
 			$(`meta[name=${name}]`).attr('content') ||
-            $(`meta[name="og:${name}"]`).attr('content') ||
-            $(`meta[name="twitter:${name}"]`).attr('content')
+			$(`meta[name="og:${name}"]`).attr('content') ||
+			$(`meta[property="og:${name}"]`).attr('content') ||
+			$(`meta[name="twitter:${name}"]`).attr('content') ||
+			$(`meta[name="twitter:${name}:src"]`).attr('content')
 		return {
 			url,
 			title: $('title').first().text(),
