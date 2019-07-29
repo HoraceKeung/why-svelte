2c881713918dd3dc838565123038403a5bfac345
Horace Keung <horace.keung@incrowdsports.com>
2019-07-29 17:45:47 +0100

    scrape one at a time

--- functions/index.js
+++ functions/index.js
@@ -3,33 +3,30 @@ const cors = require('cors')({origin: ['http://localhost:3000', 'https://why-sve
 const cheerio = require('cheerio')
 const fetch = require('node-fetch')
 
-const scrapeMetatags = urls => {
-	const requests = urls.map(async url => {
-		const res = await fetch(url).catch(err => {err})
-		const html = await res.text()
-		const $ = cheerio.load(html)
-		const getMetatag = name =>
-			$(`meta[name=${name}]`).attr('content') ||
-			$(`meta[name="og:${name}"]`).attr('content') ||
-			$(`meta[property="og:${name}"]`).attr('content') ||
-			$(`meta[name="twitter:${name}"]`).attr('content') ||
-			$(`meta[name="twitter:${name}:src"]`).attr('content')
-		return {
-			url,
-			title: $('title').first().text(),
-			favicon: $('link[rel="shortcut icon"]').attr('href'),
-			description: getMetatag('description'),
-			image: getMetatag('image'),
-			author: getMetatag('author')
-		}
-	})
-	return Promise.all(requests)
+const scrapeMetatags = async url => {
+	const res = await fetch(url).catch(err => {err})
+	const html = await res.text()
+	const $ = cheerio.load(html)
+	const getMetatag = name =>
+		$(`meta[name=${name}]`).attr('content') ||
+		$(`meta[name="og:${name}"]`).attr('content') ||
+		$(`meta[property="og:${name}"]`).attr('content') ||
+		$(`meta[name="twitter:${name}"]`).attr('content') ||
+		$(`meta[name="twitter:${name}:src"]`).attr('content')
+	return {
+		url,
+		title: $('title').first().text(),
+		favicon: $('link[rel="shortcut icon"]').attr('href'),
+		description: getMetatag('description'),
+		image: getMetatag('image'),
+		author: getMetatag('author'),
+		scrapeTime: new Date().toISOString()
+	}
 }
 
 exports.scraper = functions.https.onRequest((req, res) => {
 	cors(req, res, async () => {
-		const body = req.body
-		const data = await scrapeMetatags(body.urls)
+		const data = await scrapeMetatags(req.query.url)
 		res.send(data)
 	})
 })
--- src/App.svelte
+++ src/App.svelte
@@ -22,7 +22,7 @@
 							</div>
 							{/each}
 						</div>
-						{#if loading}
+						{#if r.loading}
 						<div class="mx-auto mb-4"><div class="loader"></div></div>
 						{:else}
 						<p class="leading-tight mb-6">{r.description || ''}</p>
@@ -38,212 +38,35 @@
 	</div>
 </div>
 
+
 <script>
-let resources = [
-	{
-		title: 'Introduction to Svelte',
-		url: 'https://daveceddia.com/svelte-intro/',
-		date: '24 Jul 2019',
-		tags: [
-			'text',
-			'vue',
-			'react',
-			'angular'
-		]
-	},
-	{
-		title: 'Web Development Simplified with Svelte',
-		url: 'https://objectcomputing.com/resources/publications/sett/july-2019-web-dev-simplified-with-svelte',
-		date: '24 Jul 2019',
-		tags: [
-			'text',
-			'sapper',
-			'vue',
-			'react',
-			'angular',
-			'nativescript'
-		]
-	},
-	{
-		title: 'Svelte for Angular Developers',
-		url: 'https://blog.bitsrc.io/svelte-for-angular-developers-34012b93420c',
-		date: '24 Jul 2019',
-		tags: [
-			'text',
-			'vue',
-			'react',
-			'angular'
-		]
-	},
-	{
-		title: 'Svelte: Why You Should Take a Look',
-		url: 'http://mandax.com.br/post/svelte-why-you-should-take-a-look/',
-		date: '22 Jul 2019',
-		tags: [
-			'text'
-		]
-	},
-	{
-		title: 'Exploring Sapper + Svelte: A quick tutorial',
-		url: 'https://blog.logrocket.com/exploring-sapper-svelte-a-quick-tutorial/',
-		date: '2 Jul 2019',
-		tags: [
-			'text',
-			'sapper',
-			'react'
-		]
-	},
-	{
-		title: "Svelte is the most beautiful web framework I've ever seen",
-		url: 'https://dev.to/jesseskinner/svelte-is-the-most-beautiful-web-framework-i-ve-ever-seen-325f',
-		date: '29 Jun 2019',
-		tags: [
-			'text'
-		]
-	},
-	{
-		title: 'Why SvelteJS may be the best framework for new web devs',
-		url: 'https://dev.to/bholmesdev/why-sveltejs-may-be-the-best-framework-for-new-web-devs-205i',
-		date: '28 Jun 2019',
-		tags: [
-			'text',
-			'vue',
-			'react',
-			'angular'
-		]
-	},
-	{
-		title: 'Getting started with Svelte',
-		url: 'https://blog.knoldus.com/getting-started-with-svelte/',
-		date: '27 Jun 2019',
-		tags: [
-			'text',
-			'vue',
-			'react'
-		]
-	},
-	{
-		title: 'Why Svelte is the best JS "framework"',
-		url: 'https://www.youtube.com/watch?v=rMBusx68ujY',
-		date: '26 Jun 2019',
-		tags: [
-			'video'
-		]
-	},
-	{
-		title: 'The importance of elegance',
-		url: 'https://www.varvet.com/blog/the-importance-of-elegance/',
-		date: '25 Jun 2019',
-		tags: [
-			'text'
-		]
-	},
-	{
-		title: "Svelte 3's radical new approach to web frameworks",
-		url: 'https://devmode.fm/episodes/svelte-3s-radical-new-approach-to-web-frameworks',
-		date: '24 Jun 2019',
-		tags: [
-			'audio'
-		]
-	},
-	{
-		title: "Reactive user interfaces using SvelteJS",
-		url: 'https://denisjakus.com/reactive-user-interfaces-using-sveltejs/',
-		date: '19 Jun 2019',
-		tags: [
-			'text'
-		]
-	},
-	{
-		title: 'Svelte.js: Two way binding',
-		url: 'https://medium.com/@Roli_Dori/svelte-js-two-way-binding-ded98a89af1c',
-		date: '25 May 2019',
-		tags: [
-			'text',
-			'vue',
-			'react'
-		]
-	},
-	{
-		title: 'Truly reactive programming with Svelte 3.0',
-		url: 'https://blog.logrocket.com/truly-reactive-programming-with-svelte-3-0-321b49b75969/',
-		date: '21 May 2019',
-		tags: [
-			'text',
-			'react',
-			'angular'
-		]
-	},
-	{
-		title: 'Svelte vs React vs Angular vs Vue',
-		url: 'https://www.youtube.com/watch?v=DZyWNS4fVE0',
-		date: '15 May 2019',
-		tags: [
-			'video',
-			'vue',
-			'react',
-			'angular'
-		]
-	},
-	{
-		title: 'Svelte 3 Front-End Framework Moves Reactivity into the JavaScript Language, Q&A with Rich Harris',
-		url: 'https://www.infoq.com/news/2019/05/svelte-3-interview-rich-harris/',
-		date: '6 May 2019',
-		tags: [
-			'text',
-			'sapper',
-			'vue',
-			'react',
-			'nativescript'
-		]
-	},
-	{
-		title: 'Svelte 3 Is All Kinds Of Awesome...',
-		url: 'https://www.youtube.com/watch?v=efAMY1JvrcQ',
-		date: '30 Apr 2019',
-		tags: [
-			'video'
-		]
-	},
-	{
-		title: 'Svelte 3 Reaction & QuickStart Tutorial',
-		url: 'https://www.youtube.com/watch?v=043h4ugAj4c',
-		date: '25 Apr 2019',
-		tags: [
-			'video',
-			'vue',
-			'react',
-			'angular'
-		]
-	},
-	{
-		title: 'Rich Harris - Rethinking reactivity',
-		url: 'https://www.youtube.com/watch?v=AdNJ3fydeao',
-		date: '22 Apr 2019',
-		tags: [
-			'video',
-			'sapper',
-			'vue',
-			'react',
-			'nativescript'
-		]
+import {set, get} from 'idb-keyval'
+
+export let resources
+
+let totalScrape = 0
+const init = async () => {
+	const resourcesInDb = await get('resources')
+	if (resourcesInDb) {
+		resources = resources.map(resource => {
+			const resourceInDb = resourcesInDb.find(x => x.url === resource.url)
+			return resourceInDb ? {...resource, ...resourceInDb} : resource
+		})
 	}
-]
-let loading = true
-fetch(scraperUrl, {
-	method: 'POST',
-	headers: {
-		'Content-Type': 'application/json'
-	},
-	body: JSON.stringify({
-		urls: resources.map(resource => resource.url)
+	resources.forEach((resource, index) => {
+		if (!resource.scrapeTime || (new Date() - new Date(resource.scrapeTime)) / 1000 / 60 / 60 / 24 > 30) {
+			totalScrape++
+			resources[index].loading = true
+			fetch(scraperUrl + `?url=${resource.url}`).then(res => res.json()).then(r => {
+				resources[index] = {...resource, ...r, loading: false}
+				totalScrape--
+				if (!totalScrape) set('resources', resources)
+			})
+		}
 	})
-}).then(res => res.json()).then(r => {
-	loading = false
-	resources = resources.map((resource, index) => {
-		return {...resource, ...r[index]}
-	})
-})
+}
+init()
+
 let searchWord = ''
 $: searchedResources = resources.filter(r => JSON.stringify(r).toLowerCase().includes(searchWord.toLowerCase()))
 </script>
